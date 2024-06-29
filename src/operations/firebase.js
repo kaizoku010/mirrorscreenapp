324896881e677e4731649702c4540e1407d06d52
Muneza Dixon <dixontheworldvsy@gmail.com>
2024-06-29 10:33:01 +0300

    Update firebase.js

--- src/operations/firebase.js
+++ src/operations/firebase.js
@@ -32,25 +32,12 @@ import {getStorage, uploadBytes,ref, getDownloadURL, uploadBytesResumable } from
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//     apiKey: "AIzaSyA689XXkcCzM-Q4GTSNrHbI9e13-9XKHoA",
-//     authDomain: "selfcheckinapps.firebaseapp.com",
-//     databaseURL: "https://selfcheckinapps-default-rtdb.firebaseio.com",
-//     projectId: "selfcheckinapps",
-//     storageBucket: "selfcheckinapps.appspot.com",
-//     messagingSenderId: "861079724058",
-//     appId: "1:861079724058:web:fcbd3d5fb16938e6a6f1a3"
+
 //   };
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC7jhaOeDPOnWJ43rCjz7eSEMav79pNU9k",
-  authDomain: "selfcheckinapps-b7264.firebaseapp.com",
-  databaseURL: "https://selfcheckinapps-b7264-default-rtdb.firebaseio.com",
-  projectId: "selfcheckinapps-b7264",
-  storageBucket: "selfcheckinapps-b7264.appspot.com",
-  messagingSenderId: "898290487365",
-  appId: "1:898290487365:web:b0cc1f2325d188ffc1132c"
+
 };
   
 // Initialize Firebase app
